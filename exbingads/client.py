2c582e15f8d235b1b498b4d9f79a8b93c6c540c2
pocin <nemeth.robin@gmail.com>
2017-10-12 11:16:54 +0800

    Add client for actually downloading the reports

--- exbingads/client.py
+++ exbingads/client.py
@@ -158,52 +158,230 @@ class AuthClient:
             response_uri=response_uri)
 
 
-            else:
-                output_bing_ads_webfault_error(api_errors)
-        elif hasattr(ex.fault, 'detail') \
-            and hasattr(ex.fault.detail, 'EditorialApiFaultDetail') \
-            and hasattr(ex.fault.detail.EditorialApiFaultDetail, 'OperationErrors') \
-            and hasattr(ex.fault.detail.EditorialApiFaultDetail.OperationErrors, 'OperationError'):
-            api_errors=ex.fault.detail.EditorialApiFaultDetail.OperationErrors.OperationError
-            if type(api_errors) == list:
-                for api_error in api_errors:
-                    output_bing_ads_webfault_error(api_error)
-            else:
-                output_bing_ads_webfault_error(api_errors)
-                # Handle serialization errors e.g. The formatter threw an exception while trying to deserialize the message: 
-                # There was an error while trying to deserialize parameter https://bingads.microsoft.com/CampaignManagement/v10:Entities.
-        elif hasattr(ex.fault, 'detail') \
-            and hasattr(ex.fault.detail, 'ExceptionDetail'):
-            api_errors=ex.fault.detail.ExceptionDetail
-            if type(api_errors) == list:
-                for api_error in api_errors:
-                    logging.error(api_error.Message)
-            else:
-                logging.error(api_errors.Message)
+
+class Client(AuthClient):
+    # https://msdn.microsoft.com/en-us/library/bing-ads-reporting-reporttimeperiod.aspx
+    predefined_times = [
+        "Today", "Yesterday", "LastSevenDays", "ThisWeek", "LastWeek",
+        "LastFourWeeks", "ThisMonth", "LastMonth", "LastThreeMonths",
+        "LastSixMonths", "ThisYear", "LastYear"
+    ]
+    # from here
+    # https://msdn.microsoft.com/en-us/library/bing-ads-reporting-adperformancereportcolumn.aspx
+    perf_report_columns = ["AccountName", "AccountNumber", "AccountId",
+                           "CampaignName", "CampaignId", "AdGroupName",
+                           "AdId", "AdGroupId", "AdTitle", "AdDescription",
+                           "AdType", "CurrencyCode", "AdDistribution", "Impressions",
+                           "Clicks", "Ctr", "AverageCpc", "Spend", "AveragePosition",
+                           "Conversions", "ConversionRate", "CostPerConversion",
+                           "DestinationUrl", "DeviceType", "Language", "DisplayUrl",
+                           "BusinessListingId", "BusinessListingName", "BusinessCategoryId",
+                           "BusinessCategoryName", "AdStatus", "Network", "TopVsOther",
+                           "BidMatchType", "DeliveredMatchType", "DeviceOS", "Assists",
+                           "Revenue", "ReturnOnAdSpend", "CostPerAssist", "RevenuePerConversion",
+                           "RevenuePerAssist", "TrackingTemplate", "CustomParameters",
+                           "FinalURL", "FinalMobileURL", "FinalAppURL", "AccountStatus",
+                           "CampaignStatus", "AdGroupStatus", "TitlePart1", "TitlePart2",
+                           "Path1", "Path2"]
+
+
+
+    def __init__(self, *auth_args, **auth_kwargs):
+        """
+
+        This client implements the actual methods to communicate with the API
+
+        Args:
+            developer_token: got from bingads registration
+            client_id (str): got during app registration
+            client_secret (str): secret for oauth2
+            refresh_token: If you have one, otherwise you will be prompted for
+                consent
+            customer_id:  get from bingads ui
+            account_id:  get from bingads ui
+            environment: 'production' or 'sandbox'
+            _username: sandbox username
+            _password: sandbox password
+            _devkey: sandbox developer_key
+
+        """
+        super().__init__(*auth_args, **auth_kwargs)
+
+        self.performance_report_fname = 'performance_report_{account_id}.csv'
+        self.timeout_ms = 20 * 60 * 1000  # 20 minutes - is that enough??
+        if self._environment == 'production':
+            self.authenticate_with_oauth()
+        elif self._environment == 'sandbox':
+            self._authenticate_with_username(
+                user_name=self._username,
+                password=self._password,
+                developer_token=self._devkey
+            )
         else:
-            raise Exception('Unknown WebFault')
+            raise ValueError("Unknown environment!")
 
-    def _output_bulk_campaigns(self, bulk_entities):
-        for entity in bulk_entities:
-            logging.error("BulkCampaign: \n")
-            logging.error("Campaign Name: {0}".format(entity.campaign.Name))
-            logging.error("Campaign Id: {0}".format(entity.campaign.Id))
+        self.reporting_service = ServiceClient(
+            service='ReportingService',
+            authorization_data=self.authorization_data,
+            version=11)
 
-            if entity.has_errors:
-                output_bulk_errors(entity.errors)
+        self.reporting_service_manager = ReportingServiceManager(
+            self.authorization_data,
+            poll_interval_in_milliseconds=5000,
+            environment=self._environment)
 
-            logging.error('')
+        # self.bulk_service_manager = BulkServiceManager(
+        #     authorization_data=self.authorization_data,
+        #     poll_interval_in_milliseconds=5000,
+        #     environment=self._environment, )
 
-    def _output_bulk_errors(self, errors):
-        for error in errors:
-            if error.error is not None:
-                logging.error("Number: {0}".format(error.error))
-                logging.error("Error: {0}".format(error.number))
-            if error.editorial_reason_code is not None:
-                logging.error("EditorialTerm: {0}".format(error.editorial_term))
-                logging.error("EditorialReasonCode: {0}".format(error.editorial_reason_code))
-                logging.error("EditorialLocation: {0}".format(error.editorial_location))
-                logging.error("PublisherCountries: {0}".format(error.publisher_countries))
-                logging.error('')
+        # self.campaign_service = ServiceClient(
+        #     service='CampaignManagementService',
+        #     authorization_data=self.authorization_data,
+        #     environment=self._environment,
+        #     version=11, )
 
+        # self.customer_service = ServiceClient(
+        #     'CustomerManagementService',
+        #     authorization_data=self.authorization_data,
+        #     environment=self._environment,
+        #     version=11, )
 
+    def _custom_date_range(self, day, month, year):
+        custom_date_range = self.reporting_service.factory.create('Date')
+        custom_date_range.Day = day
+        custom_date_range.Month = month
+        custom_date_range.Year = year
+        return custom_date_range
+
+    def _get_ad_performance_report_request(
+            self,
+            predefined_time=None,
+            complete_data=False,
+            start_date=None,
+            end_date=None,
+            columns=None,
+            aggregation='Daily',
+            report_name='Ad_Performance'):
+        '''
+        Build a keyword performance report request, including Format, ReportName, Aggregation,
+        Scope, Time, Filter, and Columns.
+
+        '''
+
+        logging.info("Building AdPerformance report request")
+        report_request = self.reporting_service.factory.create('AdPerformanceReportRequest')
+        report_request.Format = 'Csv'
+        report_request.ReportName = report_name
+        report_request.ReturnOnlyCompleteData = complete_data
+        report_request.Aggregation = aggregation
+        report_request.Language = 'English'
+
+        scope = self.reporting_service.factory.create(
+            'AccountThroughAdGroupReportScope')
+        scope.AccountIds = {'long': [self.authorization_data.account_id]}
+        scope.Campaigns = None
+        scope.AdGroups = None
+        report_request.Scope = scope
+
+        report_time = self.reporting_service.factory.create('ReportTime')
+        # You may either use a custom date range or predefined time.
+
+        # TODO parse config start date here
+
+        # can supply either predefined time or start/end date
+        # if both are defined, the predefined time has priority over custom
+        if predefined_time is not None:
+            if predefined_time in self.predefined_times:
+                logging.info(
+                    "Building report with predefinedTime %s", predefined_time)
+                report_time.PredefinedTime = predefined_time
+            else:
+                raise ValueError("predefined_time must be one of '{}'\n"
+                                 "not '{}'".format(self.predefined_times,
+                                                   predefined_time))
+        else:
+            yesterday = (datetime.datetime.utcnow() - datetime.timedelta(days=1)).date()
+            if start_date:
+                start_dt = datetime.strptime(start_date, '%d-%m-%Y').date()
+                logging.info(
+                    "Building report with startTime %s", start_dt)
+                custom_date_range_start = self._custom_date_range(
+                    start_dt.day, start_dt.month, start_dt.year)
+                report_time.CustomDateRangeStart = custom_date_range_start
+            else:
+                raise ValueError(
+                    "report start_date is not defined,"
+                    "use format %d-%m-%Y, or define `predefined_time`")
+
+            if end_date:
+                end_dt = datetime.strptime(end_date, '%d-%m-%Y').date()
+                logging.info(
+                    "Building report with endTime %s", end_dt)
+                custom_date_range_start = self._custom_date_range(
+                    end_dt.day, end_dt.month, end_dt.year)
+            else:
+                custom_date_range_end = self._custom_date_range(
+                    yesterday.day, yesterday.month, yesterday.year)
+                # Default to today
+                logging.info(
+                    "Building report with endTime %s (yesterday),"
+                    " because endTime parameter is not set.", yesterday)
+                report_time.CustomDateRangeEnd = custom_date_range_end
+
+        report_request.Time = report_time
+
+        report_columns = self.reporting_service.factory.create(
+            'ArrayOfAdPerformanceReportColumn')
+        # available columns here
+
+        report_columns.AdPerformanceReportColumn.append(
+            columns or self.perf_report_columns)
+
+        report_request.Columns = report_columns
+
+        return report_request
+
+    @log_soap_errors
+    def download_ad_performance_report(self,
+                                       outdir='/tmp/exbingads_reports',
+                                       report_filename='AdPerformance.csv',
+
+                                       start_date=None,
+                                       end_date=None,
+                                       complete_data=True,
+                                       columns=None,
+                                       aggregation='Daily'):
+        """Send a request for downloading the report, wait for completition
+
+        Also create a manifest for uploading to storage
+        """
+        try:
+            os.makedirs(outdir)
+        except FileExistsError:
+            logging.debug("outdir %s already exists", outdir)
+            pass
+        logging.info("Downloading performance report for account_id %s",
+                     self.account_id)
+        report_request = self._get_ad_performance_report_request(
+            predefined_time=predefined_time,
+            start_date=start_date,
+            end_date=end_date,
+            complete_data=complete_data,
+            aggregation=aggregation)
+
+        reporting_download_parameters = ReportingDownloadParameters(
+            report_request=report_request,
+            result_file_directory=outdir,
+            result_file_name=report_filename,
+            overwrite_result_file=True,
+            timeout_in_milliseconds=self.timeout_ms)
+
+        logging.info("Initiating download")
+        outpath = self.reporting_service_manager.download_file(
+            reporting_download_parameters)
+        if outpath is not None:
+            logging.info("No data to download!!!")
+        else:
+            logging.info("Report downloaded to %s", outpath)
+        return outpath
